dadd3f88fee7bdcbf39894d57fb298b99f3d8f66
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-14 20:53:20 +0300

    add main.py task3

--- task3/main.py
+++ task3/main.py
@@ -1,1 +1,12 @@
+class Solution:
+    def jump_by_index(self, nums):
+        index = 0
+        while index < len(nums) - 1:
+            if nums[index] == 0:
+                return False
+            index += nums[index]
+        return index == len(nums) - 1
 
+solution = Solution()
+print(solution.jump_by_index([2, 3, 1, 1, 4]))
+print(solution.jump_by_index([3, 2, 1, 0, 4]))
