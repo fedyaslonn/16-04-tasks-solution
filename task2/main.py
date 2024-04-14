5911685bbc87b3ab28f4f341e462568c4a31fb99
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-14 20:52:09 +0300

    add main.py task2

--- task2/main.py
+++ task2/main.py
@@ -1,1 +1,20 @@
-add main.py task2
+class Solution:
+    def solution(self, strs):
+        res_dict = {}
+        res = []
+        for word in strs:
+            sorted_words = ''.join(sorted(word))
+            if sorted_words in res_dict:
+                res_dict[sorted_words].append(word)
+            else:
+                res_dict[sorted_words] = [word]
+        for i in res_dict.values():
+            res.append(i)
+        return res
+
+
+sol = Solution()
+print(sol.solution(["eat","tea","tan","ate","nat","bat"]))
+print(sol.solution([""]))
+print(sol.solution(["a"]))
+
